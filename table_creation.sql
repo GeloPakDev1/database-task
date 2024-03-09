582e4a16f8ed7906393607cfd331d8aa63de07cb
GeloPakDev1 <145379929+GeloPakDev1@users.noreply.github.com>
2024-03-09 21:40:54 +0500

    fix(fk-constraints): Update FK constraints for tables

--- table_creation.sql
+++ table_creation.sql
@@ -10,41 +10,6 @@ CREATE TABLE students (
     update_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Create table for subjects
-CREATE TYPE COURSE_LEVEL_ENUM AS ENUM ('Beginner', 'Intermediate', 'Advanced');
-
-CREATE TABLE subjects (
-    id               UUID PRIMARY KEY,
-    subject_name     VARCHAR(50) NOT NULL,
-    tutor            VARCHAR(50) NOT NULL,
-    course_level     COURSE_LEVEL_ENUM NOT NULL,
-    create_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Create table for exam results
-CREATE TABLE exam_results (
-    id               UUID PRIMARY KEY,
-    student_id       UUID REFERENCES students(id),
-    subject_id       UUID REFERENCES subjects(id),
-    mark             INTEGER NOT NULL,
-    create_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Create table for enrollments
-CREATE TYPE SEMESTER_ENUM AS ENUM ('Spring', 'Summer', 'Fall', 'Winter');
-
-CREATE TABLE enrollments (
-    id UUID PRIMARY KEY,
-    student_id UUID REFERENCES students(id),
-    subject_id UUID REFERENCES subjects(id),
-    academic_year INTEGER NOT NULL,
-    semester VARCHAR(10) NOT NULL,
-    create_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    update_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
 -- Create table for tutors
 CREATE TABLE tutors (
     id UUID PRIMARY KEY,
@@ -54,6 +19,56 @@ CREATE TABLE tutors (
     qualification VARCHAR(100),
     create_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     update_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create table for subjects
+CREATE TYPE COURSE_LEVEL_ENUM AS ENUM ('Beginner', 'Intermediate', 'Advanced');
+
+CREATE TABLE subjects (
+    id               UUID PRIMARY KEY,
+    subject_name     VARCHAR(50) NOT NULL,
+    tutor_id         UUID,
+    course_level     COURSE_LEVEL_ENUM NOT NULL,
+    create_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_tutor
+        FOREIGN KEY (tutor_id)
+        REFERENCES tutors(id)
+);
+
+-- Create table for exam results
+CREATE TABLE exam_results (
+    id               UUID PRIMARY KEY,
+    student_id       UUID,
+    subject_id       UUID,
+    mark             INTEGER NOT NULL,
+    create_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_student
+        FOREIGN KEY (student_id)
+        REFERENCES students(id),
+    CONSTRAINT fk_subject
+        FOREIGN KEY (subject_id)
+        REFERENCES subjects(id)
+);
+
+-- Create table for enrollments
+CREATE TYPE SEMESTER_ENUM AS ENUM ('Spring', 'Summer', 'Fall', 'Winter');
+
+CREATE TABLE enrollments (
+    id UUID PRIMARY KEY,
+    student_id UUID,
+    subject_id UUID,
+    academic_year INTEGER NOT NULL,
+    semester VARCHAR(10) NOT NULL,
+    create_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    update_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_student
+        FOREIGN KEY (student_id)
+        REFERENCES students(id),
+    CONSTRAINT fk_subject
+        FOREIGN KEY (subject_id)
+        REFERENCES subjects(id)
 );
 
 -- Create table for departments
@@ -68,7 +83,10 @@ CREATE TABLE departments (
 CREATE TABLE courses (
     id UUID PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL,
-    department_id UUID REFERENCES departments(id),
+    department_id UUID ,
     create_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    update_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    update_datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_department
+        FOREIGN KEY (department_id)
+        REFERENCES departments(id)
 );
